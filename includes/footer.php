130b7d3905a20d35c0487ff83e1114b55a30c461
Pierre Henrion <henrion.pierre@gmail.com>
2019-03-23 03:21:21 +0100

    include js

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!-- Scripts & Plugins -->
 <script src="vendors/plugins.min.js?v=1"></script>
-<script src="dist/library/app.min.js?v=1"></script>
+<script src="library/main.js?v=1"></script>
 <!-- Scripts & Plugins -->
 </body>
 </html>
