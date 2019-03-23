be449bb43df88f98ef91747c5d01a22bd6686b7f
benjaminDelbar <b.delbar@xsi.io>
2019-03-23 14:47:14 +0100

    done

--- app/index.php
+++ app/index.php
@@ -1,11 +1,15 @@
 <?php
 include('fake_dao/RideDAO.php');
+
+include('dao/Connect.php');
 include('dao/DAO.php');
 include('dao/ChildDAO.php');
 include('dao/AccountableDAO.php');
+
 include('models/Child.php');
 include('models/Accountable.php');
 include('models/Ride.php');
+
 include('controllers/Router.php');
 
 $router = new Router($_SERVER['REQUEST_URI'], $_GET, $_POST);
