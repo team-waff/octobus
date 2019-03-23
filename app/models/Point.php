8c96022f1c2828a40c7b91a0e6ad9a68285e13d0
benjaminDelbar <b.delbar@xsi.io>
2019-03-23 22:31:09 +0100

    coucou

--- app/models/Point.php
+++ app/models/Point.php
@@ -4,9 +4,9 @@ class Point {
     var $pk;
     var $lat;
     var $lng;
-    public function __construct($pk, $lat, $lng) {
+    public function __construct($pk, $latitude, $longitude) {
         $this->pk = $pk;
-        $this->lat = $lat;
-        $this->lng = $lng;
+        $this->lat = $latitude;
+        $this->lng = $longitude;
     }
 }
