d19ebec276f75dd292f0a5817be11c0b93be1018
alexandre lacroix <alex@imacdealexandre.umons.ac.be>
2019-03-23 03:59:22 +0100

    yala

---
+++ enfant.php
@@ -0,0 +1,20 @@
+<?php
+// PAGENAME
+$page = "enfant";
+?>
+
+<?php 
+// HEADER
+include('includes/header.php');
+?>
+
+<section class="global global--enfant">
+	<p class="yaya">Hello</p>
+</section>
+
+<?php 
+// FOOTER
+include('includes/footer.php');
+?>
+
+
