f847abe770c8f5f2f26ca7973db36bac1dac0158
Pierre Henrion <henrion.pierre@gmail.com>
2019-03-23 03:19:48 +0100

    fix marker map

--- data/trajet.php
+++ data/trajet.php
@@ -22,8 +22,17 @@ $coords = array(
 );
 
 $myObj = new stdClass();
-$myObj->lat = $coords[$random]["lat"];
-$myObj->lng = $coords[$random]["lng"];
+
+// start pos
+$myObj->start_lat = "50.7185525";
+$myObj->start_lng = "4.6111773";
+
+// current pos
+$myObj->now_lat = $coords[$random]["lat"];
+$myObj->now_lng = $coords[$random]["lng"];
+
+// bus status : waiting | riding | stop | error
+$myObj->status = "riding";
 
 $myJSON = json_encode($myObj);
 
--- library/main.js
+++ library/main.js
@@ -13,42 +13,64 @@ $(document).ready(function() {
 
     if($(".global--parent").is(":visible")){
 
-		var mymap_parent = L.map('map_parent').setView([50.7185525,4.6102546], 20);
+    	$.getJSON('data/trajet.php').done(function(response) {
 
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		    maxZoom: 22,
-		    id: 'mapbox.streets',
-		    accessToken: 'pk.eyJ1Ijoib2N0b2J1cyIsImEiOiJjanRrcmt2cnQxdTU5NDRteGluMnpxM2p1In0.Zm24PvVRygwc5BoipCjvMg'
-		}).addTo(mymap_parent);
+    		// start coords
 
-		var i = 0;
+    		var start_lat = response.start_lat;
+    		var start_lng = response.start_lng;
 
-		setInterval(function(){
+    		// init
 
+			var mymap_parent = L.map('map_parent').setView([start_lat,start_lng], 20);
 
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			    maxZoom: 22,
+			    id: 'mapbox.streets',
+			    accessToken: 'pk.eyJ1Ijoib2N0b2J1cyIsImEiOiJjanRrcmt2cnQxdTU5NDRteGluMnpxM2p1In0.Zm24PvVRygwc5BoipCjvMg'
+			}).addTo(mymap_parent);
 
-			var greenIcon = L.icon({
-			    iconUrl: 'marker_start.png',
-			    shadowUrl: 'marker_start_shadow.png',
-			    iconSize:     [38, 95],
-			    shadowSize:   [50, 64],
-			    iconAnchor:   [22, 94],
-			    shadowAnchor: [4, 62],
+			// start icon
+
+			var start_icon = L.icon({
+			    iconUrl: 'graphics/marker_start.png',
+			    shadowUrl: 'graphics/marker_start_shadow.png',
+			    iconSize:     [50, 50],
+			    shadowSize:   [50, 50],
+			    iconAnchor:   [25, 25],
+			    shadowAnchor: [25, 25],
 			    popupAnchor:  [-3, -76]
-			});			
+			});	
 
-			$.getJSON('data/trajet.php').done(function(response) {
-				L.circle([response.lat,response.lng], {
-				    color: 'red',
-				    fillColor: '#f03',
-				    fillOpacity: 1,
-				    radius: 1
-				}).addTo(mymap_parent);
-				mymap_parent.panTo([response.lat, response.lng]);
-			});
+			L.marker([start_lat,start_lng], {icon: start_icon}).addTo(mymap_parent);
 
-		},2000);
+			// refresh
+
+			setInterval(function(){
+
+					$.getJSON('data/trajet.php').done(function(response) {
+
+						var status_bus = response.status;
+						console.log(status_bus);		
+
+						if(status_bus=="riding"){
+
+							L.circle([response.now_lat,response.now_lng], {
+							    color: 'red',
+							    fillColor: '#f03',
+							    fillOpacity: 1,
+							    radius: 0.2
+							}).addTo(mymap_parent);
+							mymap_parent.panTo([response.now_lat, response.now_lng]);
+
+						}
+
+					});
+
+			},2000);
+
+    	});
 
 	}
 
