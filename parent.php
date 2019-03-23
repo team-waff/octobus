9d69e74450d0e3ed1edb037e272b24924284fc15
Arno Van de Vijver <arno.vdv@icloud.com>
2019-03-23 16:15:09 +0100

    map

--- parent.php
+++ parent.php
@@ -3,18 +3,29 @@
 $page = "parent";
 ?>
 
-<?php 
+<?php
 // HEADER
 include('includes/header.php');
 ?>
 
 <section class="global global--parent">
+
 	<div class="map map--parent" id="map_parent"></div>
+
+	<div class="map_header">
+		<div class="map_header__perso">
+			<p class="map_header__name">Name</p>
+			<p class="map_header__icon"><img src="graphics/avatar_1.png" alt="" class="img_100"></p>
+		</div>
+	</div>
+
+	<div class="notif notif--visible">
+		<div class="notif__popup">Notification</div>
+	</div>
+
 </section>
 
-<?php 
+<?php
 // FOOTER
 include('includes/footer.php');
 ?>
-
-
