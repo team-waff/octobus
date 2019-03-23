243ac634d53bcf3756b611da173781bc4de61e5d
Arno Van de Vijver <arno.vdv@icloud.com>
2019-03-23 09:50:50 +0100

    popup

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,15 @@
+<a href="#" class="js_open_popup" data-popup="popup_1">Open popup</a>
+
+<div class="popup" data-popup="popup_1">
+    <div class="popup__container">
+
+        popup
+
+        <span class="popup__close js_close_popup" data-popup="popup_1"></span>
+    </div>
+    <span class="popup__bg js_close_popup" data-popup="popup_1"></span>
+</div>
+
 <!-- Scripts & Plugins -->
 <script src="vendors/plugins.min.js?v=1"></script>
 <script src="library/main.js?v=1"></script>
--- library/menu.js
+++ library/menu.js
@@ -21,4 +21,31 @@ $(document).ready(function() {
         $('.menu').addClass('menu--open');
     });
 
+/*  ==========================================================================
+    Popup
+    ==========================================================================  */
+
+    function openPopup(popup) {
+        $('body').addClass('no_scroll');
+        $('.popup').removeClass('popup--visible');
+        $('.popup[data-popup='+popup+']').addClass('popup--visible');
+    };
+
+    function closePopup(popup) {
+        $('body').removeClass('no_scroll');
+        $('.popup[data-popup='+popup+']').removeClass('popup--visible');
+    };
+
+    $('.js_open_popup').on('click', function(e){
+        e.preventDefault();
+        var popup = $(this).data('popup');
+        openPopup(popup);
+    });
+
+    $('.js_close_popup').on('click', function(e){
+        e.preventDefault();
+        var popup = $(this).data('popup');
+        closePopup(popup);
+    });
+
 });
