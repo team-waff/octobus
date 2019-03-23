f104d69a4826f94133c362e0c6897425cd1cdde7
benjaminDelbar <b.delbar@xsi.io>
2019-03-23 03:05:59 +0100

    controls

---
+++ app/controllers/controllerFactory.php
@@ -0,0 +1,11 @@
+<?php
+class controllerFactory {
+    function __construct() {
+        echo 'in controllerFactory constructor';
+    }
+
+    function get($controller, $data) {
+        echo 'in generate controller';
+
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,1 +1,21 @@
-<?php echo 'hey' ?>
+<?php
+include('controllers/controllerFactory.php');
+$split_uri = explode("/", $_SERVER['REQUEST_URI']);
+$_bypass = ['octobus', 'app', ''];
+$_models = ['accountable'];
+$parsed_uri = array();
+$controller = new controllerFactory();
+
+foreach ($split_uri as $v) {
+    if(!in_array($v, $_bypass)) {
+        array_push($parsed_uri, $v);
+    }
+}
+
+if(in_array($parsed_uri[0], $_models)) {
+    $model = $parsed_uri[0];
+    unset($parsed_uri[0]);
+    $controller->get($model, $parsed_uri[0]);
+}
+
+var_dump($parsed_uri);
